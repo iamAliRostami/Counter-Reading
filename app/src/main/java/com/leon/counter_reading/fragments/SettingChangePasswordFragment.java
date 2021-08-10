989739e67ab431563a52867baf64d44ebf356fa7
Ali Rostami <34213546+iamAliRostami@users.noreply.github.com>
2021-08-10 20:59:13 +0430

    improve change password setting.

--- app/src/main/java/com/leon/counter_reading/fragments/SettingChangePasswordFragment.java
+++ app/src/main/java/com/leon/counter_reading/fragments/SettingChangePasswordFragment.java
@@ -1,45 +1,20 @@
 package com.leon.counter_reading.fragments;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.leon.counter_reading.R;
-import com.leon.counter_reading.activities.LoginActivity;
 import com.leon.counter_reading.databinding.FragmentSettingChangePasswordBinding;
-import com.leon.counter_reading.enums.DialogType;
-import com.leon.counter_reading.enums.ProgressType;
-import com.leon.counter_reading.enums.SharedReferenceKeys;
-import com.leon.counter_reading.enums.SharedReferenceNames;
-import com.leon.counter_reading.infrastructure.IAbfaService;
-import com.leon.counter_reading.infrastructure.ICallback;
-import com.leon.counter_reading.infrastructure.ICallbackError;
-import com.leon.counter_reading.infrastructure.ICallbackIncomplete;
-import com.leon.counter_reading.infrastructure.ISharedPreferenceManager;
-import com.leon.counter_reading.tables.PasswordInfo;
-import com.leon.counter_reading.tables.SimpleResponse;
-import com.leon.counter_reading.utils.custom_dialogue.CustomDialog;
-import com.leon.counter_reading.utils.CustomErrorHandling;
 import com.leon.counter_reading.utils.CustomToast;
-import com.leon.counter_reading.utils.HttpClientWrapper;
-import com.leon.counter_reading.utils.NetworkHelper;
-import com.leon.counter_reading.utils.SharedPreferenceManager;
+import com.leon.counter_reading.utils.updating.ChangePassword;
 
 import org.jetbrains.annotations.NotNull;
-import org.json.JSONObject;
-
-import retrofit2.Call;
-import retrofit2.Response;
-import retrofit2.Retrofit;
 
 public class SettingChangePasswordFragment extends Fragment {
     FragmentSettingChangePasswordBinding binding;
@@ -56,13 +31,12 @@ public class SettingChangePasswordFragment extends Fragment {
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = com.leon.counter_reading.databinding.FragmentSettingChangePasswordBinding.inflate(inflater, container, false);
+        binding = FragmentSettingChangePasswordBinding.inflate(inflater, container, false);
         activity = getActivity();
         initialize();
         return binding.getRoot();
     }
 
-    @SuppressLint("UseCompatLoadingForDrawables")
     void initialize() {
         binding.imageViewPassword.setImageDrawable(
                 ContextCompat.getDrawable(activity, R.drawable.img_change_password));
@@ -89,8 +63,11 @@ public class SettingChangePasswordFragment extends Fragment {
                 view = binding.editTextNewPasswordConfirm;
             }
             if (!cancel) {
-                if (binding.editTextNewPassword.getText().toString().equals(binding.editTextNewPasswordConfirm.getText().toString()))
-                    attemptChangePassword();
+                if (binding.editTextNewPassword.getText().toString().equals(
+                        binding.editTextNewPasswordConfirm.getText().toString()))
+                    new ChangePassword(activity, binding.editTextOldPassword.getText().toString(),
+                            binding.editTextNewPassword.getText().toString(),
+                            binding.editTextNewPasswordConfirm.getText().toString());
                 else {
                     binding.editTextNewPassword.setError(getString(R.string.password_and_confirm_not_same));
                     binding.editTextNewPasswordConfirm.setError(getString(R.string.password_and_confirm_not_same));
@@ -101,65 +78,10 @@ public class SettingChangePasswordFragment extends Fragment {
         });
     }
 
-    void attemptChangePassword() {
-        ISharedPreferenceManager sharedPreferenceManager = new SharedPreferenceManager(activity, SharedReferenceNames.ACCOUNT.getValue());
-        Retrofit retrofit = NetworkHelper.getInstance(sharedPreferenceManager.getStringData(SharedReferenceKeys.TOKEN.getValue()));
-        IAbfaService iAbfaService = retrofit.create(IAbfaService.class);
-        PasswordInfo passwordInfo = new PasswordInfo(binding.editTextOldPassword.getText().toString(),
-                binding.editTextNewPassword.getText().toString(),
-                binding.editTextNewPasswordConfirm.getText().toString());
-        Call<SimpleResponse> call = iAbfaService.changePassword(passwordInfo);
-        HttpClientWrapper.callHttpAsync(call, ProgressType.SHOW.getValue(), activity, new ChangePassword(),
-                new ChangePasswordIncomplete(), new ChangePasswordError());
-    }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding.imageViewPassword.setImageDrawable(null);
-    }
-
-    class ChangePassword implements ICallback<SimpleResponse> {
-        @Override
-        public void execute(Response<SimpleResponse> response) {
-            if (response.body() != null)
-                new CustomToast().success(response.body().message, Toast.LENGTH_LONG);
-            Intent intent = new Intent(activity, LoginActivity.class);
-            startActivity(intent);
-            activity.finish();
-        }
-    }
-
-    class ChangePasswordIncomplete implements ICallbackIncomplete<SimpleResponse> {
-        @Override
-        public void executeIncomplete(Response<SimpleResponse> response) {
-            CustomErrorHandling customErrorHandlingNew = new CustomErrorHandling(activity);
-            String error = customErrorHandlingNew.getErrorMessageDefault(response);
-            if (response.code() == 400 && response.errorBody() != null) {
-                try {
-                    JSONObject jObjError = new JSONObject(response.errorBody().string());
-                    error = jObjError.getString("message");
-                    new CustomToast().error(error, Toast.LENGTH_LONG);
-                } catch (Exception e) {
-                    Log.e("error", e.toString());
-                }
-            } else
-                new CustomDialog(DialogType.Yellow, activity, error,
-                        activity.getString(R.string.dear_user),
-                        activity.getString(R.string.change_password),
-                        activity.getString(R.string.accepted));
-        }
-    }
-
-    class ChangePasswordError implements ICallbackError {
-        @Override
-        public void executeError(Throwable t) {
-            CustomErrorHandling customErrorHandlingNew = new CustomErrorHandling(activity);
-            String error = customErrorHandlingNew.getErrorMessageTotal(t);
-            new CustomDialog(DialogType.Yellow, activity, error,
-                    activity.getString(R.string.dear_user),
-                    activity.getString(R.string.change_password),
-                    activity.getString(R.string.accepted));
-        }
     }
 }
---
+++ app/src/main/java/com/leon/counter_reading/utils/updating/ChangePassword.java
@@ -0,0 +1,106 @@
+package com.leon.counter_reading.utils.updating;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.leon.counter_reading.R;
+import com.leon.counter_reading.activities.LoginActivity;
+import com.leon.counter_reading.enums.DialogType;
+import com.leon.counter_reading.enums.ProgressType;
+import com.leon.counter_reading.enums.SharedReferenceKeys;
+import com.leon.counter_reading.enums.SharedReferenceNames;
+import com.leon.counter_reading.infrastructure.IAbfaService;
+import com.leon.counter_reading.infrastructure.ICallback;
+import com.leon.counter_reading.infrastructure.ICallbackError;
+import com.leon.counter_reading.infrastructure.ICallbackIncomplete;
+import com.leon.counter_reading.infrastructure.ISharedPreferenceManager;
+import com.leon.counter_reading.tables.PasswordInfo;
+import com.leon.counter_reading.tables.SimpleResponse;
+import com.leon.counter_reading.utils.CustomErrorHandling;
+import com.leon.counter_reading.utils.CustomToast;
+import com.leon.counter_reading.utils.HttpClientWrapper;
+import com.leon.counter_reading.utils.NetworkHelper;
+import com.leon.counter_reading.utils.SharedPreferenceManager;
+import com.leon.counter_reading.utils.custom_dialogue.CustomDialog;
+
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class ChangePassword {
+    public ChangePassword(Activity activity, String oldPassword, String newPassword, String newPasswordConfirm) {
+        ISharedPreferenceManager sharedPreferenceManager = new SharedPreferenceManager(activity, SharedReferenceNames.ACCOUNT.getValue());
+        Retrofit retrofit = NetworkHelper.getInstance(sharedPreferenceManager.getStringData(SharedReferenceKeys.TOKEN.getValue()));
+        IAbfaService iAbfaService = retrofit.create(IAbfaService.class);
+        PasswordInfo passwordInfo = new PasswordInfo(oldPassword, newPassword, newPasswordConfirm);
+        Call<SimpleResponse> call = iAbfaService.changePassword(passwordInfo);
+        HttpClientWrapper.callHttpAsync(call, ProgressType.SHOW.getValue(), activity,
+                new Change(activity), new ChangeIncomplete(activity), new ChangeError(activity));
+    }
+}
+
+class Change implements ICallback<SimpleResponse> {
+    Activity activity;
+
+    public Change(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public void execute(Response<SimpleResponse> response) {
+        if (response.body() != null)
+            new CustomToast().success(response.body().message, Toast.LENGTH_LONG);
+        Intent intent = new Intent(activity, LoginActivity.class);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+}
+
+class ChangeIncomplete implements ICallbackIncomplete<SimpleResponse> {
+    Context context;
+
+    public ChangeIncomplete(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void executeIncomplete(Response<SimpleResponse> response) {
+        CustomErrorHandling customErrorHandlingNew = new CustomErrorHandling(context);
+        String error = customErrorHandlingNew.getErrorMessageDefault(response);
+        if (response.code() == 400 && response.errorBody() != null) {
+            try {
+                JSONObject jObjError = new JSONObject(response.errorBody().string());
+                error = jObjError.getString("message");
+                new CustomToast().error(error, Toast.LENGTH_LONG);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else
+            new CustomDialog(DialogType.Yellow, context, error,
+                    context.getString(R.string.dear_user),
+                    context.getString(R.string.change_password),
+                    context.getString(R.string.accepted));
+    }
+}
+
+class ChangeError implements ICallbackError {
+    Context context;
+
+    public ChangeError(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void executeError(Throwable t) {
+        CustomErrorHandling customErrorHandlingNew = new CustomErrorHandling(context);
+        String error = customErrorHandlingNew.getErrorMessageTotal(t);
+        new CustomDialog(DialogType.Yellow, context, error,
+                context.getString(R.string.dear_user),
+                context.getString(R.string.change_password),
+                context.getString(R.string.accepted));
+    }
+}
